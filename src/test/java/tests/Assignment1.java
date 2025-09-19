036a1c0e80f930308e8653529885766a130114f4
Chirag Jani <chiragkumarj@meditab.com>
2025-09-19 16:40:29 +0530

    Added new Test and Locator

--- src/test/java/tests/Assignment1.java
+++ src/test/java/tests/Assignment1.java
@@ -2,14 +2,23 @@ package tests;
 
 import static org.testng.Assert.assertEquals;
 
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 import browser.login.Login;
 import browser.login.central.Central;
+import browser.login.central.quickcapExecute.QuickCapExecute;
 import reusables.Elements;
 
 public class Assignment1 extends LaucnBrowser {
+	
+	SoftAssert softAssert = new SoftAssert();
+	
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofMinutes(1));
 	
 	Elements elements = new Elements();
 	
@@ -38,6 +47,23 @@ public class Assignment1 extends LaucnBrowser {
 		central.openExecuteScript();
 		
 		Assert.assertEquals(true, true);
+		
+		softAssert.assertAll();
+		
+	}
+	
+	@Test(priority=4,enabled=true,dependsOnMethods = "verifyExecuteButton")
+	public void verifyProductDropDown() {
+		
+		String currentURL = driver.getCurrentUrl();
+		String expectedURL = "https://imsaxes.meditab.com/#/Execute/SANITY/EditConfigSanity";
+		
+		softAssert.assertEquals(currentURL, expectedURL);
+		
+		QuickCapExecute quickcapExecute = new QuickCapExecute(driver);
+		
+		quickcapExecute.selectQuickCap();
+		
 		
 		
 	}
